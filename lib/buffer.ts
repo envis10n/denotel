d0cf8bc6d71e9db91da0ebe7e0e429f47d7d8f77
envis10n <envis10n@envis10n.dev>
2022-02-20 23:01:18 -0600

    Move bytes dep to a deps folder. Add Buffer class. Add TelnetParser class. Add test for Buffer.

---
+++ lib/buffer.ts
@@ -0,0 +1,63 @@
+import * as _bytes from "../deps/bytes.ts";
+
+export class Buffer {
+  private _cursor = 0;
+  private _internal: Uint8Array;
+  constructor(length: number = 0) {
+    this._internal = new Uint8Array(length);
+  }
+  public static from(arr: Uint8Array): Buffer;
+  public static from(arr: number[]): Buffer;
+  public static from(arr: Uint8Array | number[]): Buffer {
+    if (!(arr instanceof Uint8Array)) arr = new Uint8Array(arr);
+    const buf = new Buffer(arr.byteLength);
+    buf.put(arr);
+    return buf;
+  }
+  public reserve(size: number) {
+    const temp = this._internal.slice(0, this._cursor);
+    this._internal = new Uint8Array(this._internal.byteLength + size);
+    _bytes.copy(temp, this._internal);
+  }
+  public get capacity(): number {
+    return this._internal.byteLength;
+  }
+  public get length(): number {
+    return this._cursor;
+  }
+  public put(src: Uint8Array): void;
+  public put(src: number[]): void;
+  public put(src: Uint8Array | number[]) {
+    if (!(src instanceof Uint8Array)) {
+      src = new Uint8Array(src);
+    }
+    if (this._cursor + src.byteLength > this._internal.byteLength) {
+      throw new Error("Insufficient capacity");
+    }
+    _bytes.copy(src, this._internal, this._cursor);
+    this._cursor += src.byteLength;
+  }
+  public split(): Buffer {
+    const temp = this._internal.slice(0, this._cursor);
+    this._internal = new Uint8Array(this._internal.byteLength - this._cursor);
+    this._cursor = 0;
+    return Buffer.from(temp);
+  }
+  public resize(size: number) {
+    if (size > this._internal.byteLength) {
+      this.reserve(size - this._internal.byteLength);
+    } else if (size < this._internal.byteLength) {
+      const temp = this._internal.slice(0, size);
+      this._internal = temp;
+      if (this._cursor > this._internal.byteLength) {
+        this._cursor = this._internal.byteLength;
+      }
+    }
+  }
+  public freeze(): Uint8Array {
+    return this._internal.slice(0, this._cursor);
+  }
+  public clone(): Buffer {
+    return Buffer.from(this._internal.slice());
+  }
+}
--- mod.ts
+++ mod.ts
@@ -1,5 +1,29 @@
 /**
  * DenoTel
- * 
+ *
  * A telnet parser library for Deno.
  */
+
+import * as _bytes from "./deps/bytes.ts";
+import { Buffer } from "./lib/buffer.ts";
+
+export class TelnetParser {
+  private _internal_buffer: Buffer;
+  constructor(public readonly bufferSize: number = 128) {
+    this._internal_buffer = new Buffer(this.bufferSize);
+  }
+  public process(bytes: Uint8Array) {
+    if (
+      this._internal_buffer.capacity <
+        bytes.byteLength + this._internal_buffer.length
+    ) {
+      this._internal_buffer.reserve(
+        bytes.byteLength + this._internal_buffer.length -
+          this._internal_buffer.capacity,
+      );
+    }
+    this._internal_buffer.put(bytes);
+    const buf = this._internal_buffer.split().freeze();
+    // TODO: Parse buffer here
+  }
+}
